cc571c6bdbad35c6a43c550354a68269719710ab
EC2 Default User <ec2-user@ip-172-31-26-91.ap-northeast-1.compute.internal>
2019-11-07 11:37:47 +0000

    work with slibc on amazon linux

--- with_slibc.sh
+++ with_slibc.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -x
 
+sudo yum install git gcc-c++
+
 wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/slibc/slibc-0.9.2.tar.gz
 tar xfvz slibc-0.9.2.tar.gz
 cd slibc-0.9.2
@@ -9,7 +11,7 @@ make
 cd ..
 
 export LD_LIBRARY_PATH=./slibc-0.9.2/src/
-gcc -l./slibc -L./ test_all.c -lslibc
+gcc -L./slibc-0.9.2/src test_all.c -lslibc
 
 ./a.out
 
