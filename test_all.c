250897b94b5950e371317a433655c7ca284742d6
uganikov <a1901sa@aiit.ac.jp>
2019-11-14 08:00:20 +0900

    Update test_all.c
    tested with VC

--- test_all.c
+++ test_all.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 
 #define __is_not_safeclib__
 #ifdef __is_safeclib__
@@ -6,6 +5,7 @@
 #include "../safeclib/include/safe_lib.h"
 
 #else
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #endif
@@ -17,15 +17,22 @@ void test_getenv() {
 	 もちろん、良い子は dup してから保存するが、レースコンディションもあり得る。
 	 getenv_s() を利用するとそのあたりの諸々も問題が解決可能
 	 しかし、例によってパラメータとして与えるバッファ長コピー対象のデータ長より長くないとエラーになるため、とても使いにくい。
-	 バッファ長だけを返すインタフェースもないので、とてつもなく使いにくい。厳密にやるならば、ロックを取って getenv() して、文字数数えて、 malloc() だが、
-	 そこまでするならロック取って getenv() して、 dup で十分。
-	 なお getenv_s() はバッファ超不足のときは落ちるのではなくて ERANGE を返す模様。
+	 なお getenv_s() はバッファ超不足のときは落ちるのではなくて ERANGE を返したうえ、必用なバッファ長は取得できる模様。
 	 */
- 
-	size_t len;
+
+	size_t len=0;
 	char buf[1024];
 	int r = getenv_s(&len, buf, sizeof(buf), "PATH");
 	printf("%d %d %s\n", r, len, buf);
+	len = 0;
+	char buf2[16];
+	r = getenv_s(&len, buf2, sizeof(buf2), "PATH");
+	printf("%d %d %s\n", r, len, buf2);
+	char* buf3;
+	r = getenv_s(&len, NULL, 0, "PATH");
+	buf3 = malloc(len);
+	r = getenv_s(&len, buf3, len, "PATH");
+	printf("%d %d %s\n", r, len, buf3);
 }
 
 void test_gets() {
@@ -35,17 +42,27 @@ void test_gets() {
 	 実装のない向きは、 fgets() が代用可能
 	 */
 	char buf[16];
-	gets(buf);
-	printf("%s\n", buf);
-	gets_s(buf, sizeof(buf));
-	printf("%s\n", buf);
+	char buf2[16];
+	char* buf4gets =(buf>buf2)?buf2:buf;
+	printf("buf: %p buf2: %p\n", buf, buf2);
+	memset(buf, 0, sizeof(buf));
+	memset(buf2, 0, sizeof(buf2));
+	gets(buf4gets);
+	printf("gets : buf : '%s'\n", buf);
+	printf("gets : buf2: '%s'\n", buf2);
+	memset(buf, 0, sizeof(buf));
+	memset(buf2, 0, sizeof(buf2));
 	/* fgets を使う場合は末尾が改行ではない場合はバッファが足りなかったはず。*/
-	fgets(buf, sizeof(buf), stdin);
+	fgets(buf4gets, sizeof(buf), stdin);
+	printf("fgets: buf : '%s'\n", buf);
+	printf("fgets: buf2: '%s'\n", buf2);
+	gets_s(buf4gets, sizeof(buf));
+	printf("%s\n", buf);
 }
 
-void test_printf() {
+void test_sprintf() {
 	/*
-	 みんな大好き printf() 件 sprintf_s 
+	 みんな大好き printf() 件 sprintf_s
 	 sprintf_s はエラーになる。。
 	 実装のない向きは、 snprintf() が代用可能
 	snprintf はバッファが足りない場合はバッファ分書く、
@@ -53,12 +70,19 @@ void test_printf() {
 	char str[16];
 	//	strcpy_s(str, 13, "Hello World!");
 	//	sprintf(str, "1234567890123456");
-	snprintf(str, sizeof(str), "1234567890123456");
+	snprintf(str, sizeof(str), "12345678901234567890");
 	printf(str);
-	sprintf_s(str, sizeof(str), "1234567890123456");
+	sprintf_s(str, sizeof(str), "12345678901234567890");
 	printf(str);
 }
 
+void test_printf() {
+	int n;
+	printf("hogehoge%n\n", &n);
+	printf("%d\n", n);
+}
+
 int main(void) {
+	test_getenv();
 	return 0;
 }
