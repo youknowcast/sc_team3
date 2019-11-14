8c7eed23c53c2b0b6f4499769e1f208a49fab358
youknowcast <ykc@daycrift.net>
2019-11-14 17:28:22 +0900

    update with_safeclib script

--- with_safeclib.sh
+++ with_safeclib.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 set -x
 
-sudo yum install -y git automake libtool
+#sudo yum install -y git automake libtool
 
-cd ~/tmp
+#cd ~/tmp
 git clone https://github.com/rurban/safeclib
-git checkout e939e2ae4958280717add4ea3c1db617c3bf81e7
-
-# get safeclib
 cd safeclib
-#./build-aux/autogen.sh
-#./configure
-#make
+git checkout e939e2ae4958280717add4ea3c1db617c3bf81e7
+./build-aux/autogen.sh
+./configure
+make
 
-cd ~/tmp/sc_team3
+cd ..
 cp -a test_all.c test_all.c_bak
 sed -i -e 's/_not//' test_all.c
 gcc ../safeclib/src/.libs/libsafec-3.5.a test_all.c -o with_safeclib
